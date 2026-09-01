5212081da3d3c8310ca8c47356e7a96b8c610695
Oscar Alexis Pineda Henao <oschan2004@gmail.com>
2026-08-31 22:52:31 -0500

    feat: extend info endpoint with tools

--- Controllers/InfoController.cs
+++ Controllers/InfoController.cs
@@ -11,4 +11,11 @@ public class InfoController : ControllerBase
     {
         return Ok(new { description = "Práctica colaborativa con Git" });
     }
+
+    [HttpGet("tools")]
+    public IActionResult GetTools()
+    {
+        var tools = new[] { "Git", "GitHub", "Visual Studio Community 2026" };
+        return Ok(tools);
+    }
 }
