60e9071acc7937c4dd1d87e893b68067d05756f4
Juan Ramirez <juancho.jarc3@hmail.com>
2026-09-02 10:24:58 -0500

    feat: add members API endpoint

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamGitPractice.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MembersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetMembers()
+    {
+        var members = new[]
+        {
+            "Juliana Arenas Arias",
+            "Juan Andres Ramirez Castañeda",
+            "Heyner Mena Campaña",
+            "Camilo Ospina Hernández",
+            "Oscar Alexis Pineda Henao"
+        };
+
+        return Ok(members);
+    }
+}
