4172964b2de6cccf1ef8a3fd9fd5788ee11a29e3
Heyner Mena <heynermena315899@correo.itm.edu.co>
2026-09-02 00:12:06 -0500

    feat: extend version endpoint

--- Controllers/VersionController.cs
+++ Controllers/VersionController.cs
@@ -11,4 +11,10 @@ public class VersionController : ControllerBase
     {
         return Ok(new { version = "1.0.0" });
     }
+
+    [HttpGet("platform")]
+    public IActionResult GetPlatform()
+    {
+        return Ok(new { platform = ".NET 10", project = "TeamGitPractice" });
+    }
 }
